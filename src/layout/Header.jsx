5acc43952a8fdfa658778d4a9aa09ab50a33600c
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-12-22 19:16:36 -0300

    feat: create responsive navbar button

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,43 @@
 export function Header() {
   return (
-    <header>
-      <div>TM</div>
-      <nav>
+    <header className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div>TM</div>
+      </div>
+      <nav className="navbar-end">
+        <div className="dropdown">
+          <div className="btn btn-ghost md:hidden" role="button" tabIndex="0">
+            BUTTON
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a href="#">Projetos</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className="navbar-end hidden md:flex">
         <ul>
-          <li><a href="#">Projetos</a></li>
-          <li><a href="#">Sobre</a></li>
-          <li><a href="#">Contato</a></li>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
