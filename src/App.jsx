f6ea489d4488cdb91f08b8a63fb50384779735eb
Thalisson Menezes <thalissons.menezes@gmail.com>
2025-12-22 18:58:42 -0300

    feat: create folders structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Header } from "./layout/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <p className="text-2xl text-black">i'm coding</p>
     </>
   );
---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <header>
+      <div>TM</div>
+      <nav>
+        <ul>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
